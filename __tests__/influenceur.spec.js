f96f81d031e40f47af6c91bedd8316f1c63d9408
StéphaneCash <kikonistephane@gmail.com>
2023-03-28 15:49:05 +0200

    test unit

--- __tests__/influenceur.spec.js
+++ __tests__/influenceur.spec.js
@@ -2,37 +2,33 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Influenceur ', () => {
-    describe("getAll", () => {
-        test("Si tout se passe comme prévu, la fonction return 200 status", async () => {
-            try {
-                const response = await request(app).get("/api/influenceurs");
-                expect(response.statusCode).toBe(200);
-            } catch (error) {
-                console.log("Erreur getData ::: " + error)
-            }
-        })
+
+    it("Si tout se passe comme prévu, la fonction return 200 status", async () => {
+        try {
+            const response = await request(app).get("/api/influenceurs");
+            expect(response.statusCode).toBe(200);
+        } catch (error) {
+            console.log("Erreur getData ::: " + error)
+        }
     })
 
+    it("Une fois enregistré le status sera de 201", async () => {
+        const data = {
+            nom: "Zeus",
+            prenom: "Stéphane",
+            postnom: "Cash",
+            pseudo: "Dosa",
+            detail: "Roi grec",
+            textDetaillle: "Seigneur"
+        }
 
-    describe("create", () => {
-        test("Une fois enregistré le status sera de 201", async () => {
-            const data = {
-                nom: "Zeus",
-                prenom: "Stéphane",
-                postnom: "Cash",
-                pseudo: "Dosa",
-                detail: "Roi grec",
-                textDetaillle: "Seigneur"
+        try {
+            const response = await request(app).post("/api/influenceurs").send(data);
+            if (response) {
+                expect(response.statusCode).toBe(201);
             }
-
-            try {
-                const response = await request(app).post("/api/influenceurs").send(data);
-                if (response) {
-                    expect(response.statusCode).toBe(201);
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        })
-    });
+        } catch (error) {
+            console.log(error)
+        }
+    })
 })
